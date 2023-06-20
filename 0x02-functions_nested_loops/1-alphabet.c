b8d6445c237ee19ff753af155c3591f9e76c8ba2
ChoYenFat <boringaape@gmail.com>
2023-06-20 14:14:52 -0700

    putchar 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * Entry point
+ * prints the alphabet, in lowercase, followed by a new line
  */
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	char letter;
 
