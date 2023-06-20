c814da183a2d19b8d491f4e2459d6fd8a9bfc9fb
ChoYenFat <boringaape@gmail.com>
2023-06-20 04:58:01 -0700

    Variables corrections 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - will Print single digit numbers 0 - 10
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
